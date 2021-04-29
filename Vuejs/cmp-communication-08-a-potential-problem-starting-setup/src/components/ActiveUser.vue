7487ffb45941d0086e36762edf43ac4a85528da6
Billy Cockburn <coaxy42@gmail.com>
2021-04-29 16:04:23 +0100

    more tutorial work

---
+++ Vuejs/cmp-communication-08-a-potential-problem-starting-setup/src/components/ActiveUser.vue
@@ -0,0 +1,23 @@
+<template>
+  <section>
+    <li>
+      <h2>{{ username }}</h2>
+      <h3>{{ userage }}</h3>
+    </li>
+  </section>
+</template>
+<script>
+export default {
+  props: {
+    username: {
+      type: String,
+      required: true,
+    },
+   
+    userage: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
---
+++ Vuejs/cmp-communication-08-a-potential-problem-starting-setup/src/components/UserData.vue
@@ -0,0 +1,26 @@
+<template>
+  <form @submit.prevent="submitData">
+    <div><label>Name</label> <input type="text" v-model="enteredName" /></div>
+    <div><label>Age</label> <input type="text" v-model="enteredAge" /></div>
+
+    <div>
+      <button>Add User</button>
+    </div>
+  </form>
+</template>
+<script>
+export default {
+  emits: ['set-data'],
+  data() {
+    return {
+      enteredName: "",
+      enteredAge: "",
+    };
+  },
+  methods: {
+    submitData() {
+      this.$emit('set-data', this.enteredName, this.enteredAge);
+    },
+  },
+};
+</script>
