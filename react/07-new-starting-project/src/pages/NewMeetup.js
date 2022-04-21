e442b375f62e08c480018b6637f25326aa9bd361
Billy Cockburn <coaxy42@gmail.com>
2022-04-21 18:03:38 +0100

    finished react project

--- react/07-new-starting-project/src/pages/NewMeetup.js
+++ react/07-new-starting-project/src/pages/NewMeetup.js
@@ -1,9 +1,28 @@
+import { useHistory } from "react-router-dom";
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupsPage() {
-    return <section>
-        <h1>Add New Item</h1>
-        <NewMeetupForm />
-    </section>;
+  const history = useHistory();
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-meetup-d1efc-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+        history.replace('/');
+    } );
+  }
+  return (
+    <section>
+      <h1>Add New Item</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
 }
 export default NewMeetupsPage;
