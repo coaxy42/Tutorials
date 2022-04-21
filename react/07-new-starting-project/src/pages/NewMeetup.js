019a94c3dc364f8afe442da4ffd5a6534a51ba0c
Billy Cockburn <coaxy42@gmail.com>
2022-04-21 15:31:31 +0100

    meetup app

--- react/07-new-starting-project/src/pages/NewMeetup.js
+++ react/07-new-starting-project/src/pages/NewMeetup.js
@@ -1,4 +1,9 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetupsPage() {
-    return <div>New Meetups Page</div>;
+    return <section>
+        <h1>Add New Item</h1>
+        <NewMeetupForm />
+    </section>;
 }
 export default NewMeetupsPage;
