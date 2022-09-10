d5a334299e08d617c96f3fc138f8288ae89922fc
Billy Cockburn <coaxy42@gmail.com>
2022-09-10 22:59:21 +0100

    comit

--- react/09-time-to-practice-starting-code/src/App.js
+++ react/09-time-to-practice-starting-code/src/App.js
@@ -1,9 +1,26 @@
 import { Route, Switch, Redirect } from "react-router-dom";
+import MainHeader from "./components/layout/MainNavigation";
+import QuoteForm from "./components/quotes/QuoteForm";
+import QuoteList from "./components/quotes/QuoteList";
 
 function App() {
   return (
     <div>
-      
+      <MainHeader />
+      <main>
+        <Switch>
+        <Route path='/' exact> 
+            <Redirect to='/QuoteList' />
+          </Route>
+          <Route path="/quotes">
+            <QuoteList />
+          </Route>
+          <Route path="/addquote" exact>
+            <QuoteForm />
+          </Route>
+         
+        </Switch>
+      </main>
     </div>
   );
 }
--- react/09-time-to-practice-starting-code/src/components/quotes/QuoteItem.js
+++ react/09-time-to-practice-starting-code/src/components/quotes/QuoteItem.js
@@ -9,9 +9,7 @@ const QuoteItem = (props) => {
         </blockquote>
         <figcaption>{props.author}</figcaption>
       </figure>
-      <a className='btn'>
-        View Fullscreen
-      </a>
+     
     </li>
   );
 };
